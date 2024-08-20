b3f68d651bd6b9bd867bea4a059fa4f3173cede1
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-08-20 21:47:48 +0300

    add vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
-const { defineConfig } = require('@vue/cli-service');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' 
-  ? '/Restaurant/' 
-  : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Restaurant/'
+    : '/',
   css: {
     extract: {
       filename: 'style.css',
-      chunkFilename: 'style.css',
-    },
+      chunkFilename: 'style.css'
+    }
   },
   configureWebpack: {
     plugins: [
       new MiniCssExtractPlugin({
         filename: 'style.css',
-        chunkFilename: 'style.css',
-      }),
-    ],
-  },
-});
+        chunkFilename: 'style.css'
+      })
+    ]
+  }
+})
