ca83623cfda2b299e090d6bf7ccafe8c96a8deae
Андрей кропинов <andreikropinov17121988@gmail.com>
2024-08-21 18:54:18 +0300

    edit

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
     ? '/Restaurant/'
-    : '/',
+    : '/home',
   css: {
     extract: {
       filename: 'style.css',
